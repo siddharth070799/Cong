dbcfeae00c7dd4b3078878e49e3e515951ec9fcf
Aware Drive <awaredrive5@gmail.com>
2025-08-24 16:12:42 +0530

    day4 complete

--- www/main.js
+++ www/main.js
@@ -40,4 +40,13 @@ $(document).ready(function () {
         },
     });
 
+    // mic button click event
+
+    $("#MicBtn").click(function () { 
+        eel.playAssistantSound()
+        $("#Oval").attr("hidden", true);
+        $("#SiriWave").attr("hidden", false);
+        
+    });
+
 });
